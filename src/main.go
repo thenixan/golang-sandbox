f719a32dcb2ebf24dccd06adea33adfb4a954dc7
Ilya Nixan <thenixan@gmail.com>
2018-09-19 19:39:55 +0300

    Working change auth code

--- src/main.go
+++ src/main.go
@@ -7,4 +7,6 @@ import (
 
 func main() {
 	fmt.Println(userdatabase.CheckAuth("dial","ESXbgvqY"))
+	fmt.Println(userdatabase.ChangePassword("dial","ESXbgvqY", "ESXbgvqYa"))
+	fmt.Println(userdatabase.CheckAuth("dial","ESXbgvqY"))
 }
